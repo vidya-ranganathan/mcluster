9f650806cb4610dc83bb959196bb3c19aa95307d
vidya-ranganathan <win4vidya@gmail.com>
2024-01-25 14:02:57 +0530

    Creating CRD in types.go

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	//"context"
+	//"flag"
+	//"fmt"
+	//"log"
+	//"path/filepath"
+
+	//klient "github.com/vidya-ranganathan/mcluster/pkg/client/clientset/versioned"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//"k8s.io/client-go/rest"
+	//"k8s.io/client-go/tools/clientcmd"
+	//"k8s.io/client-go/util/homedir"
+	"fmt"
+
+	"github.com/vidya-ranganathan/mcluster/pkg/cumulonimbus.ai/v1alpha1"
+)
+
+func main() {
+	/*
+		var kubeconfig *string
+
+		if home := homedir.HomeDir(); home != "" {
+			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		} else {
+			kubeconfig = flag.String("kubeconfig", "", "absolute path to kubeconfig")
+		}
+		flag.Parse()
+
+		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			log.Printf("Building config from flags, %s", err.Error())
+			config, err = rest.InClusterConfig()
+			if err != nil {
+				log.Printf("error %s building inclusterconfig", err.Error())
+			}
+		}
+		klientset, err := klient.NewForConfig(config)
+		if err != nil {
+			log.Printf("getting klient set %s\n", err.Error())
+		}
+
+		klusters, err := klientset.CumulonimbusV1alpha1().Mclusters("").List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			log.Printf("listing klusters - %s\n", err.Error())
+		}
+
+		fmt.Printf("listing klusters %d\n", len(klusters.Items))
+	*/
+
+	mc := v1alpha1.Mcluster{}
+	fmt.Println(mc)
+
+}
---
+++ pkg/apis/cumulonimbus.ai/doc.go
@@ -0,0 +1,5 @@
+// +k8s:deepcopy-gen=package
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=cumulonimbus.ai
+
+package v1alpha1
